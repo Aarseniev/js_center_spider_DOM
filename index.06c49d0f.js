a28a3a5985fd1d4dff0db963c1db4b9e06487a39
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-06 00:16:38 +0300

    make build

---
+++ index.06c49d0f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".wall"),e=document.querySelector(".spider");function o(){var o=t.clientWidth/2,c=t.clientHeight/2,n=o-e.offsetWidth/2,i=c-e.offsetHeight/2;e.style.left="".concat(n,"px"),e.style.top="".concat(i,"px")}e.complete&&e.offsetWidth>0?o():e.addEventListener("load",o)}();
+//# sourceMappingURL=index.06c49d0f.js.map
