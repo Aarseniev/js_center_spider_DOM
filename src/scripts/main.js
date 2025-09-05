97a4e81e0f908763eae3c0b3bd00ac53ce5a9c20
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-05 21:01:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+
+function moveSpiderToCenter() {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const centerX = wall.clientHeight / 2;
+  const centerY = wall.clientWidth / 2;
+
+  const leftSpider = centerX - spider.offsetWidth / 2;
+  const topSpider = centerY - spider.offsetWidth / 2;
+
+  spider.style.left = `${leftSpider}px`;
+  spider.style.top = `${topSpider}px`;
+}
+
+moveSpiderToCenter();
